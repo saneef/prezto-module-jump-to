db153519c0c44ed03c7ca23c82d0cfe15218a0a5
Saneef Ansari <saneef@gmail.com>
2015-10-06 23:39:02 +0530

    Adding first jump paths

---
+++ init.zsh
@@ -0,0 +1,14 @@
+#
+# Defines jump-to aliases.
+#
+# Authors:
+#   Saneef Ansari <saneef@gmail.com>
+#
+
+#
+# Aliases
+#
+
+alias jwk='cd ~/workspace'
+alias jwk='cd ~/workspace/razorfish'
+alias jtmp='cd ~/tmp'
